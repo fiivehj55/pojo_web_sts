b599ce5a55a9b76be53539bc68d5079e755cf276
tslav <tslav@DESKTOP-5719799>
2016-10-05 19:02:34 +0900

    tx

--- mvc_POJO/src/main/java/com/example/service/HouseServiceImp.java
+++ mvc_POJO/src/main/java/com/example/service/HouseServiceImp.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.dao.HouseDao;
 import com.example.dao.HouseDaoImpl;
@@ -21,30 +22,39 @@ public class HouseServiceImp implements HouseService{
 	@Autowired
 	SqlSessionTemplate template;
 	
+	@Transactional
 	@Override
 	public List<House> selectById(String id) {
 		List<House> list =  null;
 		list = hdao.selectAllHouse(template);
 		return list;
 	}
+	
+	@Transactional
 	@Override
 	public House selectByNoHouse(int houseNo) {
 		House house	=  null;
 		house = hdao.selectByNoHouse(template, houseNo);
 		return house;
 	}
+	
+	@Transactional
 	@Override
 	public int insertHouse(House house) {
 		int result = 0;
 		result = hdao.insertHouse(template, house);
 		return result;
 	}
+	
+	@Transactional
 	@Override
 	public int updateHouse(House house) {
 		int result = 0;
 		result = hdao.updateHouse(template, house);
 		return result;
 	}
+	
+	@Transactional
 	@Override
 	public int deleteHouse(int houseNo) {
 		int result = 0;
